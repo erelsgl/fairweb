14cfb6dd3e5d9287746807b4c1668b92e6d57626
Erel Segal-Halevi cron <erelsgl@gmail.com>
2022-11-05 18:15:52 +0200

    both tests work

---
+++ test_gspread.py
@@ -0,0 +1,6 @@
+import gspread
+
+account = gspread.service_account("credentials.json")
+spreadsheet = account.open("TestSpreadsheet")
+sheet1 = spreadsheet.worksheet("Sheet1")
+print("Rows: ", sheet1.row_count, "Cols: ", sheet1.col_count)
