b0836c4d0ea12364e96b6c538957c4f9ee28d742
Erel Segal-Halevi <erelsgl@gmail.com>
2022-11-07 20:47:30 +0200

    flask button

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Solution from here: https://stackoverflow.com/a/49334973
+
+#rendering the HTML page which has the button
+@app.route('/')
+def json():
+    return render_template('button.html')
+
+@app.route('/background_process_test')
+def background_process_test():
+    print ("Hello")
+    return "Test complete"
+
+@app.route('/run_the_algorithm')
+def run_the_algorithm():
+    import run
+    run.run()
+    return "Run complete"
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- run.py
+++ run.py
@@ -1,28 +1,32 @@
 import input, allocate, gspread, output
 
-print("\nOPENING SPREADSHEET")
-account = gspread.service_account("credentials.json")
-spreadsheet = account.open("FairDivision")
+def run():
+    print("\nOPENING SPREADSHEET")
+    account = gspread.service_account("credentials.json")
+    spreadsheet = account.open("FairDivision")
 
-print("\nREADING INPUT DATA")
-rows = input.read_rows(spreadsheet)
-agents, items, entitlement_normalized_preferences  = input.analyze_rows(rows)
-print("agents: ", agents, "items: ", items)
+    print("\nREADING INPUT DATA")
+    rows = input.read_rows(spreadsheet)
+    agents, items, entitlement_normalized_preferences  = input.analyze_rows(rows)
+    print("agents: ", agents, "items: ", items)
 
-print("\nCOMPUTING ALLOCATION")
-map_agent_to_fractions = allocate.allocate(agents, entitlement_normalized_preferences)
-print("allocation: ", map_agent_to_fractions)
+    print("\nCOMPUTING ALLOCATION")
+    map_agent_to_fractions = allocate.allocate(agents, entitlement_normalized_preferences)
+    print("allocation: ", map_agent_to_fractions)
 
 
-print("\nUPDATING OUTPUT SHEET")
-new_row_count = len(agents)+2
-new_col_count = len(items)+5
-output_sheet = output.worksheet(spreadsheet, new_row_count, new_col_count)
-output_sheet.clear()
-new_cells = output.cells(rows, agents, items, map_agent_to_fractions)
-output_sheet.update_cells(new_cells, value_input_option='USER_ENTERED')
+    print("\nUPDATING OUTPUT SHEET")
+    new_row_count = len(agents)+2
+    new_col_count = len(items)+5
+    output_sheet = output.worksheet(spreadsheet, new_row_count, new_col_count)
+    output_sheet.clear()
+    new_cells = output.cells(rows, agents, items, map_agent_to_fractions)
+    output_sheet.update_cells(new_cells, value_input_option='USER_ENTERED')
 
-print("\nFORMATTING OUTPUT SHEET")
-first_cell = gspread.utils.rowcol_to_a1(2, 3)
-last_cell = gspread.utils.rowcol_to_a1(len(agents)+2, len(items)+5)
-output_sheet.format(f"{first_cell}:{last_cell}", {"numberFormat": {"type": "PERCENT", "pattern": "##.#%"}})
+    print("\nFORMATTING OUTPUT SHEET")
+    first_cell = gspread.utils.rowcol_to_a1(2, 3)
+    last_cell = gspread.utils.rowcol_to_a1(len(agents)+2, len(items)+5)
+    output_sheet.format(f"{first_cell}:{last_cell}", {"numberFormat": {"type": "PERCENT", "pattern": "##.#%"}})
+
+if __name__=="__main__":
+    run()
