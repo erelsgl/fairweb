11d631260b1767db0e1cf7a55fdaef2d316c51a1
Erel Segal-Halevi <erelsgl@gmail.com>
2023-08-16 13:49:40 +0300

    course allocation

--- courses/output.py
+++ courses/output.py
@@ -28,16 +28,12 @@ TEXTS = {
 		"en": "course >"
 	},
 	"capacity": {
-		"he": "מספר מקומות",
+		"he": "מקס' מקומות",
 		"en": "capacity",
 	},
-	"due_value_percent": {
-		"he": "ערך מגיע באחוזים",
-		"en": "Due value in percent",
-	},
-	"value_ratio": {
-		"he": "יחס ערכים",
-		"en": "Value ratio",
+	"seats": {
+		"he": "מקומות בפועל",
+		"en": "seats",
 	},
 }
 
@@ -51,8 +47,7 @@ def cells(input_rows, agent_capacities, item_capacities, map_agent_to_bundle, ma
 
 	AGENT_NAME_COLUMN     = 1
 	AGENT_CAPACITY_COLUMN = 2
-	AGENT_BUNDLE_COLUMN   = 3
-	AGENT_EXPLANATION_COLUMN = 4
+	AGENT_BUNDLE_COLUMN   = 4
 	INTRO_ROW = 1
 	ITEM_NAME_ROW = 2
 	ITEM_CAPACITY_ROW = 3
@@ -64,6 +59,7 @@ def cells(input_rows, agent_capacities, item_capacities, map_agent_to_bundle, ma
 	new_cells += [gspread.Cell(INTRO_ROW+1,2,text("item_name"))]
 	new_cells += [gspread.Cell(INTRO_ROW+2,1,text("agent_name"))]
 	new_cells += [gspread.Cell(INTRO_ROW+2,2,text("capacity"))]
+	new_cells += [gspread.Cell(INTRO_ROW+3,3,text("seats"))]
 	# new_cells += [gspread.Cell(i+1, AGENT_NAME_COLUMN, "=valuations!"+gspread.utils.rowcol_to_a1(i+1,AGENT_NAME_COLUMN)) for i in range(len(input_rows))]     # Copy column 1: agent names
 	# new_cells += [gspread.Cell(i+1, AGENT_CAPACITY_COLUMN, "=valuations!"+gspread.utils.rowcol_to_a1(i+1,AGENT_CAPACITY_COLUMN)) for i in range(len(input_rows))]  # Copy column 2: agent capacities
 	# new_cells += [gspread.Cell(ITEM_NAME_ROW, o+3, items[o]) for o in range(len(items))]   # Write item names
@@ -71,23 +67,27 @@ def cells(input_rows, agent_capacities, item_capacities, map_agent_to_bundle, ma
 
 	for o in range(len(items)):
 		item_o = items[o]
-		column = o+4
+		column = o+5
+		column_letter = gspread.utils.rowcol_to_a1(1, column)[:-1]
 		new_cells += [gspread.Cell(ITEM_NAME_ROW, column, item_o)]
+		new_cells += [gspread.Cell(ITEM_NAME_ROW+1, column, item_capacities[item_o])]
+		new_cells += [gspread.Cell(ITEM_NAME_ROW+2, column, f"=sum({column_letter}{ITEM_NAME_ROW+3}:{column_letter})")]
 
 	# Insert results:
 	for i in range(len(agents)):
 		agent_i = agents[i]
 		bundle_i = map_agent_to_bundle[agent_i]
 		explanation_i = map_agent_to_explanation[agent_i]
-		row = i+4
+		row = i+5
 		print(agent_i, ": ", bundle_i)
 		new_cells += [gspread.Cell(row, AGENT_NAME_COLUMN, agent_i)]
 		new_cells += [gspread.Cell(row, AGENT_CAPACITY_COLUMN, agent_capacities[agent_i])]
+		column_letter = gspread.utils.rowcol_to_a1(1, AGENT_CAPACITY_COLUMN+2)[:-1]
+		new_cells += [gspread.Cell(row, AGENT_CAPACITY_COLUMN+1, f"=sum({column_letter}{row}:{row})")]
 		new_cells += [gspread.Cell(row, AGENT_BUNDLE_COLUMN,   str(bundle_i))]
-		# new_cells += [gspread.Cell(row, AGENT_EXPLANATION_COLUMN,   str(explanation_i))]
 		for o in range(len(items)):
 			item_o = items[o]
-			column = o+4
+			column = o+5
 			fraction_i_o = 1 if item_o in bundle_i else 0
 			new_cells += [gspread.Cell(row, column, fraction_i_o)]
 
