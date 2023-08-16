74c4ec55af14a4876b7bfda648cf2296aa7f2c89
Erel Segal-Halevi <erelsgl@gmail.com>
2023-08-16 14:16:24 +0300

    remove debug mode

--- app.py
+++ app.py
@@ -77,6 +77,6 @@ def background_process_test():
     return "Test complete"
 
 if __name__ == '__main__':
-    app.run(debug = True)
-    # app.run(debug = False, host="0.0.0.0")
+    # app.run(debug = True)
+    app.run(debug = False, host="0.0.0.0")
 
